000ba03d9feb57c341c736a5e54cd35c7aa9c176
EnricAlmiñana <alminyana@gmail.com>
2014-11-06 17:32:07 +0100

    server connect and structure

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+//BD localhost connect
+mongoose.connect('mongodb://localhost:27017/extrem');
+
+//BD MongoLab connect
+//var db = "";
+
+app.use(express.static(__dirname + '/public'));
+//app.use(express.logger('dev'));
+app.use(bodyParser.json());
+
+//simulate DELETE and PUT http methods
+//app.use(express.methodOverride());
+
+
+//require('./app/routes')(app);
+
+app.listen(port, function(){
+	console.log('extrem app listening : ' + port);
+});
